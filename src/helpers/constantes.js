f6c960b7698e6f1fe9c73ad7f5e4112896380ae6
AlbertV99 <tico.urquhart@gmail.com>
2023-07-25 00:04:18 -0400

    Actualizacion de constantes

--- src/helpers/constantes.js
+++ src/helpers/constantes.js
@@ -1,6 +1,6 @@
 const Constantes = ()=>{
     // const HOST = "http://localhost/rastreo";
-    const HOST = "http://monitor.valurq.com"; 
+    const HOST = "https://monitor.valurq.com"; 
     // const HOST = "http://192.168.100.88/rastreo";
     // const HOST = "https://vmarcador.valurq.com"
 
