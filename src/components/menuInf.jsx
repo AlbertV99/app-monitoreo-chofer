ad4d5b290a73f1a9e7931b949f14fc71f463c610
AlbertV99 <tico.urquhart@gmail.com>
2025-04-24 15:06:38 -0300

    Se levanta la version

--- src/components/menuInf.jsx
+++ src/components/menuInf.jsx
@@ -1,17 +1,17 @@
-import React,{useState} from 'react'
-import PropTypes from 'prop-types'
-import {Container,Row,Col,Button,Form} from 'react-bootstrap';
-import {useNavigate,NavLink} from "react-router-dom"
-import { BiBusSchool,BiUser } from "react-icons/bi";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import { useNavigate, NavLink } from "react-router-dom";
+import { BiBusSchool, BiUser } from "react-icons/bi";
 
 const MenuInferior = (props) => {
-    const base = {"historial":"","hora":"","config":""};
-    const [dir,setDir] = useState({"historial":"","hora":"","config":""});
-    const cambiar = (direccion)=>{
+    const base = { historial: "", hora: "", config: "" };
+    const [dir, setDir] = useState({ historial: "", hora: "", config: "" });
+    const cambiar = (direccion) => {
         let temp = base;
-        temp[direccion]="activo"
+        temp[direccion] = "activo";
         setDir(temp);
-    }
+    };
     /*
     historial  BiHistory
     marcar BiCalendarCheck | BiUserCheck
@@ -19,35 +19,57 @@ const MenuInferior = (props) => {
     // registro BiUserCircle
     */
     return (
-        <Container fluid style={{padding:"0px",justifyContent: "center",backgroundColor:'white'}} >
-            <Row style={{width:"100%",justifyContent:"center"}} id="opciones">
-            <Row>
-                <Col xs={4}>
-                </Col>
-                <Col xs={8} style={{padding:'0px',textAlign:'right'}}>
-                    <p style={{fontSize:'8pt',color:'#a2a4a6',margin:'3px 0px'}}>
-                        Desarrollado por Valurq S.R.L.<span style={{fontWeight:"bold",color:'black'}}> (V.4-Beta)</span>
-                    </p>
-                </Col>
-            </Row>
+        <Container
+            fluid
+            style={{
+                padding: "0px",
+                justifyContent: "center",
+                backgroundColor: "white",
+            }}
+        >
+            <Row
+                style={{ width: "100%", justifyContent: "center" }}
+                id="opciones"
+            >
+                <Row>
+                    <Col xs={4}></Col>
+                    <Col xs={8} style={{ padding: "0px", textAlign: "right" }}>
+                        <p
+                            style={{
+                                fontSize: "8pt",
+                                color: "#a2a4a6",
+                                margin: "3px 0px",
+                            }}
+                        >
+                            Desarrollado por Valurq S.R.L.
+                            <span
+                                style={{ fontWeight: "bold", color: "black" }}
+                            >
+                                {" "}
+                                (V.5-Beta)
+                            </span>
+                        </p>
+                    </Col>
+                </Row>
                 <Col xs={6}>
                     <NavLink to={`/viajeReg`}>
-                            <h2><BiBusSchool/></h2>
-                            Viaje
+                        <h2>
+                            <BiBusSchool />
+                        </h2>
+                        Viaje
                     </NavLink>
                 </Col>
                 <Col xs={6}>
                     <NavLink to={`/perfil`}>
-
-                        <h2><BiUser/></h2>
+                        <h2>
+                            <BiUser />
+                        </h2>
                         Perfil
                     </NavLink>
-
                 </Col>
             </Row>
-
         </Container>
-    )
-}
+    );
+};
 
 export default MenuInferior;
